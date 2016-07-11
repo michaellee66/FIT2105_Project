e5ffa73e4105a58afa0f13c9353b31b9190d8f5a
Zeyu Michael Li <Michael_Lee@zeyu-michael-lis-macbook-pro.local>
2016-07-11 16:08:28 +0200

    Initial Commit

---
+++ prato/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prato
+//
+//  Created by ZEYU_Michael_Li on 11/07/2016.
+//  Copyright © 2016 ZEYU_Michael_Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
